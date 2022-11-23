71877a52d5a8a537b14f08df4572075eb3f82cfc
Hana SB <hana.su95@gmail.com>
2022-11-23 20:35:40 +0100

    first commit

---
+++ src/app/live/components/live-page/live-page.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+
+import * as Highcharts from 'highcharts/highmaps';
+import * as WorldMap from '@highcharts/map-collection/custom/world.geo.json'; 
+import { DataService } from 'src/app/shared/services/data.service';
+
+@Component({
+  selector: 'app-live-page',
+  templateUrl: './live-page.component.html',
+  styleUrls: ['./live-page.component.css']
+})
+export class LivePageComponent implements OnInit {
+
+  liveData: any[] = [];
+
+  constructor(private dataService: DataService) { }
+
+  Highcharts: typeof Highcharts = Highcharts; 
+  chartConstructor = 'mapChart'; 
+
+  chartOptions: Highcharts.Options | any = {
+    chart: {
+      map: WorldMap
+    }, 
+    title: {
+      text: 'COVID-19 World Data'
+    }, 
+    subtitle: {
+      text: 'Confirmed Cases Live'
+    }, 
+    mapNavigation: {
+      enabled: true, 
+      buttonOptions: {
+        alignTo: 'specingBox'
+      }
+    }, 
+    legend: {
+      enabled: true
+    }, 
+    colorAxis: {
+      min: 0
+    },
+    series: [{
+      type: 'map', 
+      name: 'COVID-19 Data', 
+      states: {
+        hover: {
+          color: '#dc3545'
+        }
+      }, 
+      dataLabels: {
+        enabled: true, 
+        format: '{point.name}'
+      }, 
+      allAreas: false, 
+      data: [
+      ]
+    }]
+  }
+
+  ngOnInit(): void {
+    this.dataService.getSummaryData()
+      .subscribe(
+        (data: any) => {
+          this.liveData = data.Countries.map((country: any) => [country.CountryCode.
+            toLowerCase(), country.TotalConfirmed]);
+
+            this.chartOptions.series[0].data = this.liveData;
+
+            this.Highcharts.mapChart('container', this.chartOptions);
+        }, 
+        (error: any) => {
+          console.log(error);
+        }
+      );
+  }
+
+}
